a569490c19bb1acc036fdf2cd8a681843176624e
Bijaya Krishna Das <BijayaKrishna_Das@amat.com>
2021-01-19 05:44:51 +0530

    first commit

---
+++ js/map.markers.js
@@ -0,0 +1,31 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Reception",
+		"latitude":20.0029,
+		"longitude":85.4388,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Kamaguru, Khurda <br> Odisha-752021, IN"
+	},
+	{
+		"title": "Accommodation 1",
+		"latitude":20.2961,
+		"longitude":85.8245,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
+	},
+	{
+		"title": "Transportation",
+		"latitude":20.2511,
+		"longitude":85.8153,
+		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Ceremony",
+		"latitude":20.1755,
+		"longitude":85.2800,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Chuda, Bologarh, Khurda <br> Odisha, IN 752022"
+	}
+];
